d3eddd524b596f0b782de4d2312fe34628638341
Albina <albina@Albinas-MacBook-Pro.local>
2024-03-02 23:04:39 +0100

    init

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
+export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-};
+  base: '/', // Adjust this based on your deployment environment
+});
